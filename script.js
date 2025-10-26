dc2619c59e34095af6f965cc846d75402117470f
Bannoth Trishul Naik <86940212+trishulnaik@users.noreply.github.com>
2025-10-26 09:18:55 +0000

    FIX: error to append the child at end by creating a HTML element.

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
-function createCard(vnumber, title, cName, views, monthsOld, duration ,thumbnail){
-    // finish the function
-    const videoNumber = document.querySelector(".container .index-number");
-    videoNumber.textContent = vnumber;
-    const thumbailContainer = document.querySelector(".image-container")
-    thumbailContainer.style.backgroundImage = `url(${thumbnail})`;
-    document.querySelector(".video-duration").textContent = duration;
-    const vtitle = document.querySelector(".container .video-title");
-    vtitle.textContent = title;
-    document.querySelector(".channel-name").textContent = cName;
-
-    let textViews = views;
-    if(textViews>1000000) {textViews = `${(views/1000000).toFixed(2)}M`}
-    else if(textViews<1000000 && textViews>1000){
-        textViews = `${(views/1000).toFixed(2)}K`
-    }
-
-    document.querySelector(".views").textContent = textViews;
-    document.querySelector(".uploaded").textContent = `${monthsOld} months ago`;
+function createCard(
+  vnumber,
+  title,
+  cName,
+  views,
+  monthsOld,
+  duration,
+  thumbnail
+) {
+  // finish the function
+  let textViews = views;
+  if (textViews > 1000000) {
+    textViews = `${(views / 1000000).toFixed(2)}M`;
+  } else if (textViews < 1000000 && textViews > 1000) {
+    textViews = `${(views / 1000).toFixed(2)}K`;
+  }
+  let html = `
+    <div class="item">
+        <div class="index-number">
+            ${videoNumber}
+        </div>
+        <div class="image-container">
+            <img src=${thumbnail}-${vnumber} alt=thumbnail-${videoNumber}</img>
+            <span class="video-duration">${duration}</span>
+        </div>
+        <div class="card-description">
+            <h3 class="video-title">${title}</h3>
+            <div class="desciption">
+                <span class="channel-name">${cName}</span>
+                <span class="views">${textViews}</span>
+                <span class="uploaded">${monthsOld} months ago</span>
+            </div>
+        </div>`;
+  document.querySelector(".container").innerHTML += html;
 }
 
+// creation of cards
 let videoNumber = 23;
-createCard(videoNumber, `Introduction to BackEnd | Sigma Web Development - Tutorial #${videoNumber}`, "Code with Harry", 13459000, 7, "32:34", "https://i.ytimg.com/vi/kJEsTjH5mVg/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLBYghRBiZcZs-xVJKq92lAM8h3BOg")
+createCard(
+  videoNumber,
+  `Introduction to BackEnd | Sigma Web Development - Tutorial #${videoNumber}`,
+  "Code with Harry",
+  13459000,
+  7,
+  "32:34",
+  "https://i.ytimg.com/vi/kJEsTjH5mVg/hqdefault.jpg?sqp=-oaymwEmCKgBEF5IWvKriqkDGQgBFQAAiEIYAdgBAeIBCggYEAIYBjgBQAE=&rs=AOn4CLBYghRBiZcZs-xVJKq92lAM8h3BOg"
+);
